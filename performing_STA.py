fff74bf8ec7aaa4c484da6da61906518a1e5fd9e
sergey20240124 <157232852+sergey20240124@users.noreply.github.com>
2024-06-12 19:39:46 +0300

    Update performing_STA.py

--- performing_STA.py
+++ performing_STA.py
@@ -7,7 +7,8 @@ import io
 # Suppress the specific UserWarning from openpyxl
 warnings.filterwarnings("ignore", category=UserWarning, module="openpyxl")
 
-st.title("Create a Bulk File to Upload Extracted Performing Customer Search Terms")
+st.title("Create a Bulk File to Upload Performing Search Terms")
+st.write("What exactly this app does")
 
 # ACOS input
 acos_input = st.number_input("Enter target ACOS (%)", min_value=0.0, max_value=100.0, value=15.0)
